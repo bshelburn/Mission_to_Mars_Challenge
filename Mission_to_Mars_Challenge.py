ffe8cd4fa3eeba3eb3f5bbb02ab9db5ab41067f9
bshelburn <84201082+bshelburn@users.noreply.github.com>
2021-09-18 12:17:18 -0400

    Add files via upload

---
+++ Mission_to_Mars_Challenge.py
@@ -0,0 +1,175 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+# Import Splinter and BeautifulSoup
+from splinter import Browser
+from bs4 import BeautifulSoup as soup
+from webdriver_manager.chrome import ChromeDriverManager
+import pandas as pd
+
+
+# In[2]:
+
+
+executable_path = {'executable_path': ChromeDriverManager().install()}
+browser = Browser('chrome', **executable_path, headless=False)
+
+
+# In[3]:
+
+
+# Visit the mars nasa news site
+url = 'https://redplanetscience.com'
+browser.visit(url)
+# Optional delay for loading the page
+browser.is_element_present_by_css('div.list_text', wait_time=1)
+
+
+# In[4]:
+
+
+html = browser.html
+news_soup = soup(html, 'html.parser')
+slide_elem = news_soup.select_one('div.list_text')
+
+
+# In[5]:
+
+
+slide_elem.find('div', class_='content_title')
+
+
+# In[6]:
+
+
+# Use the parent element to find the first `a` tag and save it as `news_title`
+news_title = slide_elem.find('div', class_='content_title').get_text()
+news_title
+
+
+# In[7]:
+
+
+# Use the parent element to find the paragraph text
+news_p = slide_elem.find('div', class_='article_teaser_body').get_text()
+news_p
+
+
+# ### Featured Images
+
+# In[8]:
+
+
+# Visit URL
+url = 'https://spaceimages-mars.com'
+browser.visit(url)
+
+
+# In[9]:
+
+
+# Find and click the full image button
+full_image_elem = browser.find_by_tag('button')[1]
+full_image_elem.click()
+
+
+# In[10]:
+
+
+# Parse the resulting html with soup
+html = browser.html
+img_soup = soup(html, 'html.parser')
+
+
+# In[11]:
+
+
+# Find the relative image url
+img_url_rel = img_soup.find('img', class_='fancybox-image').get('src')
+img_url_rel
+
+
+# In[12]:
+
+
+# Use the base URL to create an absolute URL
+img_url = f'https://spaceimages-mars.com/{img_url_rel}'
+img_url
+
+
+# In[13]:
+
+
+df = pd.read_html('https://galaxyfacts-mars.com')[0]
+df.columns=['description', 'Mars', 'Earth']
+df.set_index('description', inplace=True)
+df
+
+
+# In[14]:
+
+
+df.to_html()
+
+
+# In[15]:
+
+
+#browser.quit()
+
+
+# In[16]:
+
+
+#1
+url = 'https://astrogeology.usgs.gov/search/results?q=hemisphere+enhanced&k1=target&v1=Mars'
+browser.visit(url)
+
+
+# In[17]:
+
+
+#2
+hemisphere_image_urls = []
+
+
+# In[18]:
+
+
+#3
+imgs_links= browser.find_by_css("a.product-item h3")
+for x in range(len(imgs_links)):
+    hemisphere={}
+    
+    browser.find_by_css("a.product-item h3")[x].click()
+    
+    sample_img= browser.find_link_by_text("Sample").first
+    hemisphere['img_url']=sample_img['href']
+    
+    hemisphere['title']=browser.find_by_css("h2.title").text
+    
+    hemisphere_image_urls.append(hemisphere)
+    browser.back()
+
+
+# In[19]:
+
+
+#4
+hemisphere_image_urls
+
+
+# In[ ]:
+
+
+browser.quit()
+
+
+# In[ ]:
+
+
+
+
